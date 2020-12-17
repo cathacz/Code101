a217477949eef372dc93dd1c244da95f7a6038d3
catha <catharinaczipf@hotmail.com>
2020-12-17 14:59:46 +0100

    first js play

---
+++ dec/17-12/index.js
@@ -0,0 +1,68 @@
+//Printing out a sting
+console.log('Catha says:');
+console.log('"I am bored"');
+console.log('lucky'); 
+//Mathematical expression: +, -, *, /, %
+console.log(28/2);
+//Boolean expressions: ==, !==, >=,<=, >, < (Comparison operations)
+console.log(12===12)
+console.log('Catha');
+let x = 28;
+console.log(x);
+let y = 4;
+console.log(y);
+
+let sur = 'Czipf';
+
+let hBg = "stuff";
+console.log(x / y);
+x = 40;
+console.log(x/y);
+
+console.log(typeof x);
+console.log(x);
+
+
+//ternary condition
+let person = 22;
+person > 16 ? console.log('go to Party!'):console.log('stay home');
+
+let drive = 18;
+
+drive >= 18 ? console.log('get your licence'):console.log('Moped fahren');
+
+let num1 = 1;
+let num2 = "1";
+console.log(num1 === num2);
+
+//Camelcas is fine – dont do anything else
+let userName = 'Catharina';
+console.log (typeof userName);
+console.log (userName.length);
+
+let mail = 'c@hotmail.com';
+
+mail[0] == "@" ? console.log("you suck"):console.log("nicly done");
+
+
+let hi= "griasDi";
+console.log(hi);
+
+console.log(hi[2]);
+console.log(hi[6]);
+
+let name = "Catha";
+let age = 33;
+let year = 1987;
+console.log(name, 'is', age, 'and born in', year);
+console.log(age);
+let isMarried = 'yes';
+isMarried == "yes" ? console.log('sucks'):console.log('free at least');
+
+let pl ='JavaScript';
+console.log(pl[0],pl[4]);
+
+let isStudent = "yay";
+isStudent[0] == "y" ? console.log('€5 please'): console.log('€12 for you');
+let speedCheck = 120;
+speedCheck >= 50 ? console.log('too fast'):console.log('right speed');
