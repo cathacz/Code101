d6f3038f0e13a2573198a08a84267c80ab0e6820
catha <catharinaczipf@hotmail.com>
2021-01-05 10:02:15 +0100

    more stuff

---
+++ jan/05-01/index.js
@@ -0,0 +1,11 @@
+console.log(x);
+var x;
+x = null; // >> means its empty – type = object
+x = true; // >> boolean
+x = ""; // >> its a string
+x = undefined;
+console.log(typeof x);
+
+function sum (3, 5); {
+    return x + y;
+}
