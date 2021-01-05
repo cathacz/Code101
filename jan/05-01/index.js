c77a346d7e6d830b176f35f52a58505084c54249
catha <catharinaczipf@hotmail.com>
2021-01-05 10:53:13 +0100

    I like pushing

--- jan/05-01/index.js
+++ jan/05-01/index.js
@@ -1,11 +1,43 @@
-console.log(x);
-var x;
-x = null; // >> means its empty – type = object
-x = true; // >> boolean
-x = ""; // >> its a string
-x = undefined;
-console.log(typeof x);
+// console.log(x);
+// var x;
+// x = null; // >> means its empty – type = object
+// x = true; // >> boolean
+// x = ""; // >> its a string
+// x = undefined;
+// console.log(typeof x);
 
-function sum (3, 5); {
+sum(3, 4);
+function sum (x, y) {
     return x + y;
+    x+y+x+x+x+y;
 }
+console.log(sum);
+
+let x = 3;
+let y = 4;
+
+true ? console.log("true") : console.log("false");
+
+let str ="Catha";
+console.log(str[0]);
+console.log(str[str.length-2]);
+
+let longStr = "               Catha fetzt!              ";
+console.log(longStr.trim());
+
+function display (str) {
+    console.log(str);
+}
+display ("Karl-Herbert");
+// + - * /
+console.log(y % x); // >> remender 
+
+// float numbers 
+
+let int1 ="144";
+let int2 = 1.5;
+console.log(int1 + " " + int2);
+//is it NOT a number = isNaN
+isNaN (int1) ? console.log("true") : console.log("false");
+
+display(int2 * int1);
