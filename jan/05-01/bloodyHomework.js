44184f5b2b6776cb72cdfab515f0cbe01e205b51
catha <catharinaczipf@hotmail.com>
2021-01-05 17:46:43 +0100

    push it

---
+++ jan/05-01/bloodyHomework.js
@@ -0,0 +1,31 @@
+// I thought tomorrow is off and it will be nice to practice a bit more, so here are some fun exercises >> we have a different understanding from "fun" ;)
+
+// MATH – Perform Mathematical Tasks with JavaScript. Remember: you must use the Math functions and print all results to the console!
+
+function display (str) {
+    console.log(str);
+}
+display("Math-Stuff");
+
+// 1. Minimum and maximum
+display("Minimun & Maximum:");
+// a. Lowest Number >> Print out the lowest number between -1 and 4.
+display(Math.min(-1, 0, 1, 2, 3, 4));
+// b. Highest Number >> Print out the highest number between -1 and 4.
+display(Math.max(-1, 0, 1, 2, 3, 4));
+// 2. Rounding
+display("Rounding:");
+// a. Round up >> Round up each of the following numbers to the nearest integer: 3321.32321, 326.76, 76788.7, -9.78, 43.342.
+// b. Round down >> Round down each of the following numbers to the nearest integer: 3321.32321, 326.76, 76788.7, -9.78, 28.329.
+// Strings and Numbers
+display("Strings and Numbers:");
+// 1. Concatenation.
+// Create two variables. One variable should contain a string value and the other should contain a number. Concatenate the string and the number.
+// let introSentence = "Hi, my name is Nancy and I am";
+// let age = 25;
+// Expected Output:
+// "Hi, my name is Nancy and I am 25"
+// In this case, what is the + operator doing? Comment your answer in the js file.
+// 2. Converting
+// Create a variable with the value of “1005”. Convert it to a number.
+// Create a variable with the value of “10.05". Convert it to a number.
