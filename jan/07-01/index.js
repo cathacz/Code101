bc8826bb3823b99580016d884c4980d151ab2340
catha <catharinaczipf@hotmail.com>
2021-01-07 13:11:46 +0100

    fooooor meee

--- jan/07-01/index.js
+++ jan/07-01/index.js
@@ -28,7 +28,6 @@ console.log("floaty", strIntoFloat);
 
 display(((x + y) / (z + 2.44)) * 29 + x);
 
-
 // Number + String
 let d, f, r;
 d = "C";
@@ -62,14 +61,12 @@ display(typeof x);
 display(typeof console) //object > object orientated language
 display(typeof Math.ceil) // function
 
-
 // 3.444444444444444
 
 let floatNum1 = 0.1;
 let floatNum2 = 0.1;
 let = result = floatNum1 * floatNum2;
 display(result.toFixed(3));
-
 
 //Math.random
 //numbers between 0-100 > * 100 +1 | 101 >>>> just 0-99 = *100
@@ -96,7 +93,11 @@ let v = 10;
 console.log(w == v ? "cool" : "not cool"); // type doesnt matter
 console.log(w === v ? "cool" : "not cool"); //=== not both same type
 
-console.log(Math.floor(Math.random()*6)+1); //Create a program that prints a random integer from 1 - 6.
+console.log(Math.floor(Math.random() * 6) + 1); //Create a program that prints a random integer from 1 - 6.
+
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>        START 
 
 function square(h) {
     return (Math.sqrt(h * h));
@@ -105,16 +106,46 @@ function square(h) {
 
 console.log(Math.sqrt(Math.pow(4, 2) + Math.pow(2, 2))); // Pythagoras: a² + b² = c² 
 
-console.log(Math.sin(90));
+console.log("Sinus90: " + Math.sin(90));
+
+display("Why functions never work");
 
 let alpha = 45;
+let h = 1; // Hypotenuse
 
-//function sinus(num) {
-  //  let sine = Math.sqrt(Math.pow(1,2)-Math.sqrt(Math.cos(alpha),2));
-    //return Math.sin(sine);
-//}
-//console.log(sinus);
+function sinus() {
+    //return sinus = Math.sin(Math.sqrt(Math.pow(h,2)-Math.sqrt(Math.cos(alpha),2)));
+    //return sinus = Math.sin(Math.cos(alpha, 2) / Math.pow(h,2));
+    return sinus = Math.sin(Math.cos(alpha)/h);
+}
+console.log(sinus());
+
+display("check: (Sinus Alpha)");
+console.log(Math.sin(alpha));
+display(">> arcSinus:");
+console.log(Math.asin(Math.sin(alpha)));
+display("doubleCheck: (Sinus Alpha = tanAlpha * cosAlpha)");
+console.log(Math.tan(alpha) * Math.cos(alpha));
+
+
+
 display("Sinus");
-console.log(Math.sin(Math.sqrt(Math.pow(1,2)-Math.sqrt(Math.cos(alpha),2))));
-console.log(Math.sin(Math.sqrt(Math.pow(1,2)-Math.sqrt(Math.cos(89),2))));
+console.log(Math.sin(Math.sqrt(Math.pow(h, 2) - Math.sqrt(Math.cos(alpha), 2))));
+console.log(Math.sin(Math.sqrt(Math.pow(h, 2) - Math.sqrt(Math.cos(1), 2))));
 
+display(">>>>>> new Try");
+
+function gegenkathete(){
+    return gegenkathete = Math.sin(alpha) * h;
+}
+ display("Gegenkathete:" + gegenkathete());
+
+
+let gk = 4;
+let hy = 5;
+
+let sine = Math.sin(gk/hy);
+display("SinusAlpha = " + gk/hy);
+display(sine);
+
+display(Math.asin(gk/hy));
