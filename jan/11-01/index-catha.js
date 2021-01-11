1c16d4ff2d038639ef9dcdad845a6f44962d391c
catha <catharinaczipf@hotmail.com>
2021-01-11 09:51:44 +0100

    I hate Mondays

--- jan/11-01/index-catha.js
+++ jan/11-01/index-catha.js
@@ -1,10 +1,54 @@
-function print (str) {
+//"old-skool" function
+function print(str) {
     console.log(str);
 }
-
+// function in ES 6
 const display = (str) => {
     console.log(str);
 }
-
+console.log(typeof display);
 print("Hey");
 display("pretty face");
+
+
+let a = "tschibrat"
+display("hey" + "you");
+display(`hey you ${a}`);
+print(`hey you ${a}`);
+
+
+const sum = (x, y) => x + y;
+console.log(sum(3, 4));
+
+const isBigger = (num1, num2) => num1 < num2;
+console.log(isBigger(297, 7383));
+
+const printFullWord = (firstName, age, add) => console.log(`hey ${firstName}, you learned ${add} with your ${age} years. `);
+
+printFullWord("Gina", 23, "nothing");
+
+let isLogged = false;
+const login = () => {
+    isLogged = true;
+};
+login();
+
+
+let name1 = "Catha";
+let name2 = "Seppi";
+let name3 = "Alpha";
+
+// index of:        0       1          2
+let namesArr = ["Catha", "Seppi", "Alpha"];
+console.log(namesArr[2]);
+
+// index of      0  1  2  3  4  5  6  7  8  9
+const numbers = [2, 0, 8, 1, 9, 7, 3, 6, 5, 4];
+console.log(numbers[5]);
+
+const hobbies = ["sleeping", "photography", "drawing", "gardening", "building stuff"];
+let i = 0;
+console.log (hobbies[i]);
+i++;
+console.log (hobbies[i]);
+i++;
