7e06d93eef7471139aaa9b6f4611e8bb430c6975
catha <catharinaczipf@hotmail.com>
2021-01-16 21:09:32 +0100

    f*** loops

---
+++ jan/16-01.weekendStuff/password.js
@@ -0,0 +1,162 @@
+const passwordGen = () => {
+  let allCharacters = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    6,
+    7,
+    8,
+    9,
+    "!",
+    "§",
+    "$",
+    "%",
+    "&",
+    "?",
+    "@",
+    "€",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  let result = "";
+  let randomNum;
+  for (let i = 0; i <= 30; i++) {
+    randomNum = Math.floor(Math.random() * allCharacters.length); //need to be inside the loop!!
+    result += allCharacters[randomNum]; //>> result = result + allCharacters[randomNum]>> so every character is random!
+  }
+  return result;
+};
+
+console.log(`New Password: ${passwordGen()}`);
+
+// lottoZahlenGenerator >>> 6 aus 49
+
+const lottoZahlenGenerator = () => {
+  let num = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29,
+    30,
+    31,
+    32,
+    33,
+    34,
+    35,
+    36,
+    37,
+    38,
+    39,
+    40,
+    41,
+    42,
+    43,
+    44,
+    45,
+    46,
+    47,
+    49,
+  ];
+
+  let lotto = "";
+  let random;
+
+  for (i = 1; i <= 6; i++) {
+    //(i =0; i <= 5; i++) >> geht auch
+    random = Math.floor(Math.random() * num.length);
+    lotto += `${num[random]},`;
+  }
+  return lotto;
+};
+
+console.log(`Your Pick: ${lottoZahlenGenerator()}`);
+
+const lottoGen = () => {
+  let result = "";
+  let randomNum;
+  for (i = 1; i <= 6; i++) {
+    for (j = 1; j <= 49; j++) {
+      randomNum = Math.ceil(Math.random() * j); // mit Math.floor 0 dabei...
+    }
+    result += `${randomNum},`; // numbers can repeat...
+  }
+  return result;
+};
+console.log(`Your Numbers: ${lottoGen()}`);
---
+++ jan/16-01.weekendStuff/stupidLoops.js
@@ -0,0 +1,68 @@
+let x = "x";
+
+const drawSomething = () => {
+  for (i = 0; i <= 4; i++);
+  let result = +x * i;
+  return result;
+};
+console.log(drawSomething());
+
+const numbersCount = () => {
+  for (let i = 0; i <= 5; i++) {
+    let text = "";
+    for (let j = 1; j <= i; j++) {
+      text += j;
+    }
+    console.log(text);
+  }
+};
+numbersCount();
+
+const numbersCount2 = () => {
+  let text;
+  for (let i = 5; i >= 0; i--) {
+    text = "";
+    for (let j = 1; j <= i; j++) {
+      text += j;
+    }
+    console.log(text);
+  }
+};
+numbersCount2();
+
+//Romans stupid thing
+
+/*
+Hi @everyone A tiny task, if you have a desire to study a little on the weekend:smile:
+You will be given two extremely similar arrays, but exactly one of the items in an array will be valued slightly higher than its counterpart (which means that evaluating the value > the other value will return true).*/
+//Create a function that returns whether the first array is slightly superior to that of the second.
+//Worked Example
+// isFirstSuperior([1, 2, 4], [1, 2, 3]) ➞ true
+// The pair of items at each index are compared in turn.
+// 1 from the first array is the same as 1 from the second array.
+// 2 is the same as 2.
+// However, 4 is greater than 3, so first array is superior.
+/*Examples
+isFirstSuperior(["a", "d", "c"], ["a", "b", "c"]) ➞ true
+isFirstSuperior(["zebra", "ostrich", "whale"], ["ant", "ostrich", "whale"]) ➞ true
+isFirstSuperior([1, 2, 3, 4], [1, 2, 4, 4]) ➞ false
+isFirstSuperior([true, 10, "zebra"], [true, 10, "zebra"]) ➞ false    */
+/*Notes
+Both arrays will be the same length.
+All values and their counterparts will always be of the same data type.
+If the two arrays are the same, return false.*/
+
+const array1 = [1, 2, 3];
+const array2 = [1, 2, 7];
+
+const isSuperior = () => {
+  for (i = 0; i <= array1.length; i++) {
+    if (array1[i] <= array2[i]) {
+      return console.log(`true`);
+    } else {
+      return console.log(`nagl dia a gurkal ins knie`);
+    }
+  }
+};
+
+isSuperior(); // 🙄
