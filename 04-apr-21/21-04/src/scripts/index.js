9d36282e6057ebb87f2fb9f45a1eea026c71b5b5
catha <catharinaczipf@hotmail.com>
2021-04-21 15:59:06 +0200

    js in browser action

--- 04-apr-21/21-04/src/scripts/index.js
+++ 04-apr-21/21-04/src/scripts/index.js
@@ -34,3 +34,51 @@ function gimmeSqrt() {
 
   document.querySelector(".result").innerHTML = resultText;
 }
+
+// AEIOU:
+// Vowels. Create a function that takes a string in its parameters and counts the number of vowels (i.e. in English, “a, e, i, o, u”) in the string.
+// i.e. findVowels(“this is a string”) ➞ 4
+// SumOfNumbers.
+
+// const findVowels = (str) => {
+//   let vowels = ["a", "e", "i", "o", "u"];
+//   let arr = str.toLowerCase().split("");
+//   let result = 0;
+//   console.log(arr);
+//   for (let i = 0; i <= arr.length; i++) {
+//     if (arr[i].includes(vowels[i])) {
+//       result += 1;
+//     }
+
+//     return result;
+//   }
+// };
+// console.log(findVowels("Here are some vowels"));
+
+const findVowels = (str) => {
+  let arr = str.split("");
+  let result = 0;
+  let vowels = "aeiou";
+  console.log(arr);
+  for (let i = 0; i <= arr.length; i++) {
+    if (vowels.includes(arr[i])) {
+      result += 1;
+    }
+  }
+  return result;
+};
+console.log(findVowels("this is a string"));
+console.log(findVowels("Here are some vowels"));
+
+// SumOfNumbers.
+// Create a program that adds up the numbers in an array (of at least 3 numbers). Bonus: Print to screen both the sum and the product of these numbers.
+// const sumOfNum =(arr) =>{
+
+// }
+let arr = [3, 5, 9, 12];
+const reducer = arr.reduce(
+  (accumulator, currentValue) => accumulator + currentValue
+);
+console.log(reducer);
+
+// I forgot how much js sucked
