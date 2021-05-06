e6579b66dd6b1827b286084f5c86e621fe05c106
catha <catharinaczipf@hotmail.com>
2021-05-06 14:31:50 +0200

    promise promise

--- 05-may-21/06-05/fetch/scripts/index.js
+++ 05-may-21/06-05/fetch/scripts/index.js
@@ -35,23 +35,66 @@ function fetchJsonFile() {
 
 // we will use
 // https://jsonplaceholder.typicode.com/photos
+// function fetchApi() {
+//   fetch("https://jsonplaceholder.typicode.com/photos")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data);
+//       let userCard = "<h2>Images API response </h2>";
+//       data.forEach((img) => {
+//         let { albumId, id, title, url, thumbnailUrl } = img;
+//         userCard += `
+//           <div id=${id}>
+//           <img src=${thumbnailUrl} >
+//           <h3>${title}</h3>
+//           <img src=${url} alt=${title}>
+//           </div>
+//           `;
+//       });
+//       document.querySelector(".result").innerHTML = userCard;
+//     })
+//     .catch((err) => console.log(err));
+// }
+
+// -------------- TO DOs ----------------------
 function fetchApi() {
-  fetch("https://jsonplaceholder.typicode.com/photos")
+  fetch("https://jsonplaceholder.typicode.com/todos")
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      let userCard = "<h2>Images API response </h2>";
-      data.forEach((img) => {
-        let { albumId, id, title, url, thumbnailUrl } = img;
+      let userCard = "<h2>ToDos API response </h2>";
+      data.forEach((toDo) => {
+        let { userId, id, title, completed } = toDo;
         userCard += `
-          <div id=${id}>
-          <img src=${thumbnailUrl} >
-          <h3>${title}</h3>
-          <img src=${url} alt=${title}>
-          </div>
-          `;
+            <div id=${id}><h3>${title}</h3>
+            <p>${completed}</p>
+            </div>
+            `;
       });
       document.querySelector(".result").innerHTML = userCard;
     })
     .catch((err) => console.log(err));
 }
+
+// -------------- COMMENTS ----------------------
+// function fetchApi() {
+//   fetch("https://jsonplaceholder.typicode.com/comments")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data);
+//       let userCard = "<h2>Comments API response </h2>";
+//       data.forEach((comments) => {
+//         let { name, id, email, body } = comments;
+//         userCard += `
+//               <div id=${id}>
+
+//               <h3>${name}</h3>
+//               <h4>${email}</h4>
+//               <p>${body}</p>
+//               </div>
+//               `;
+//       });
+//       document.querySelector(".result").innerHTML = userCard;
+//     })
+//     .catch((err) => console.log(err));
+// }
