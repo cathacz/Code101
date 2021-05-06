75fe224fcc1bd979e8c7215931022b8d24147668
catha <catharinaczipf@hotmail.com>
2021-05-06 10:43:39 +0200

    promise promise

--- 05-may-21/06-05/index.js
+++ 05-may-21/06-05/index.js
@@ -2,8 +2,10 @@
 // Blocking code
 // blocking code(loops, console.log, loading files )
 // Non-blocking code
-window.setTimeout(console.log(`Penis`), 5);
-console.log(`Vagina`);
+window.setTimeout(() => {
+  console.log("hej you");
+}, 100);
+console.log("me first");
 // Promises
 // Promises are very similar to callbacks but they are a little bit clean on writing
 // Promises are the best solution if you want to do something that takes a long time to be done and you don't want your app to wait for it to be done, so it will work on the background, Like downloading an image or a video from another server
@@ -24,4 +26,19 @@ checkPromise
   .catch((message) => {
     console.log(`${message} sadly`);
   });
+
 // Promisify setTimeout
+function delay(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
+let ms = 3000;
+function doSomething() {
+  let text = document.querySelector(".text");
+  text.innerHTML = `it took me ${ms}ms to be loaded`;
+}
+
+delay(ms).then(doSomething);
