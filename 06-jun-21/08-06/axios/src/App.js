0f0ef5b0b7fdba0c8701c2351be8f0dce22f5390
catha <catharinaczipf@hotmail.com>
2021-06-08 11:03:31 +0200

    axios

---
+++ 06-jun-21/08-06/axios/src/App.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./scss/main.scss";
+import Loading from "./components/Loading";
+import Country from "./components/Country";
+const App = () => {
+  const [userInput, setUserInput] = useState("");
+  const [result, setResult] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  function handleChange(e) {
+    setUserInput(e.target.value);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    let textToURl = encodeURIComponent(userInput);
+    let endPoint = `https://restcountries.eu/rest/v2/name/${textToURl}`;
+
+    // old school fetch: (much more then axios)
+    // fetch(endPoint).then((res)=>res.json()).then((data)=>setResult(data));
+
+    axios(endPoint)
+      .then(({ data }) => setResult(data))
+      .catch(console.error(`Nope – you took a wrong turn somewhere`));
+  }
+  //   State          Component
+  if (loading) return <Loading />;
+  return (
+    <React.Fragment>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={userInput}
+          onChange={handleChange}
+          placeholder="country?"
+        />
+        <button type="submit">Search</button>
+      </form>
+      <Country result={result} />
+    </React.Fragment>
+  );
+};
+
+export default App;
