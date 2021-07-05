bd159f47133ace0bf2b7c6651b1988f206d39f5d
catha <catharinaczipf@hotmail.com>
2021-07-05 10:06:41 +0200

    they said I should push more

--- 07-jul-21/05-07/components/src/App.js
+++ 07-jul-21/05-07/components/src/App.js
@@ -1,6 +1,44 @@
 import React from "react";
+import { add, display } from "./helpers";
+// >>> function component: <<<
+// function App() {
+//   return <React.Fragment></React.Fragment>;
+// }
 
-function App() {
-  return <React.Fragment></React.Fragment>;
+// >>> class component <<<
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      arr: [],
+      isLogged: false,
+      data: { name: "Catha", age: 31 },
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  print = () => console.log("I am a ES6 function and I dont need to be bind");
+
+  handleSubmit() {
+    console.log("I need to be bind > connected to constructor");
+  }
+
+  render() {
+    // render is a MUST in class based components
+    this.newUser = () => console.log("I live inside the render function");
+    return (
+      <div>
+        {" "}
+        {display("3+7=")}
+        {this.print()}
+        {this.handleSubmit()}
+        {this.newUser()}
+        <h1>Hej there {this.state.data.name}</h1>
+        <p>
+          3+4=<b>{add(3, 4)}</b>
+        </p>
+      </div>
+    );
+  }
 }
-export default App;
+// export default App;
---
+++ 07-jul-21/05-07/components/src/helpers.js
@@ -0,0 +1,3 @@
+const add = (x, y) => x + y;
+const display = (str) => console.log(str);
+export { add, display };
