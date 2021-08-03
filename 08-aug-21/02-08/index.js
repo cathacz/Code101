f392d0a458d360bfd96cc598b4a99b55f5964554
catha <catharinaczipf@hotmail.com>
2021-08-03 09:03:06 +0200

    start backend

--- 08-aug-21/02-08/index.js
+++ 08-aug-21/02-08/index.js
@@ -7,8 +7,8 @@ process.argv.forEach((item, i) => {
   console.log(`${i}: ${item}`);
 });
 // remember and do this:
-const args = process.argv.slice(2);
-console.log(args);
+// const args = process.argv.slice(2);
+// console.log(args);
 
 // >>> fs (File System) built-in module in NodeJS
 
@@ -31,3 +31,58 @@ fs.writeFile(
     console.log("Done");
   }
 );
+
+fs.writeFile(
+  "./anotherText.txt",
+  "so I created another File with a meaningless Text inside...",
+  function (err) {
+    if (err) throw err;
+    console.log("There you go");
+  }
+);
+fs.readFile("./anotherText.txt", "utf8", function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
+const args = process.argv.slice(2);
+let stars = args[0];
+let header = args[1];
+const print = require("./star");
+print(stars, header);
+
+//  >>> Calculator
+function sum(numArray) {
+  return numArray.reduce((sum, n) => sum + n, 0);
+}
+function avg(numArray) {
+  return sum(numArray) / numArray.length;
+}
+function parsNum(arg) {
+  const number = parseFloat(arg);
+  if (isNaN(number)) {
+    console.log(`Sorry, ${arg} is not a number. Start over.`);
+    process.exit();
+  }
+  return number;
+}
+// more advanced then let stars = args[1]:
+const [operation, ...rest] = args;
+const numbers = rest.map(parsNum);
+// sum 2 4 f
+
+// avg 89 64 98787 0 29292
+
+function med(numArray) {
+  switch (operation) {
+    case "sum":
+      console.log(sum(numArray));
+      break;
+    case "avg":
+      console.log(avg(numArray));
+      break;
+    default:
+      console.log("Nope!");
+  }
+}
+med(numbers);
---
+++ 08-aug-21/02-08/star.js
@@ -0,0 +1,3 @@
+module.exports = (stars = 10, header = "twinkle, twinkle") => {
+  console.log(`${"*".repeat(stars)}\n${header}\n${"*".repeat(stars)}`);
+};
