deec05bfa540467553ef8ac81c8466825e2465f9
catha <catharinaczipf@hotmail.com>
2021-12-05 20:59:03 +0100

    alles mal

---
+++ 11-Nov/typeScript/index.js
@@ -0,0 +1,15 @@
+// TypeScript -> A JavaScript superset
+console.log("Hej, I am TypeScript");
+let x = 0;
+x = 6;
+let w = "";
+w = "Word";
+// function add(x: number, y: number): number {
+//   return x + y;
+// }
+const add = (x, y) => {
+    return x + y;
+};
+const result = add(2, 5);
+console.log(result);
+console.log("Penis");
---
+++ 11-Nov/typeScript/index.ts
@@ -0,0 +1,21 @@
+// TypeScript -> A JavaScript superset
+console.log("Hej, I am TypeScript");
+
+let x: number = 0;
+
+x = 6;
+
+let w: string = "";
+w = "Word";
+
+// function add(x: number, y: number): number {
+//   return x + y;
+// }
+
+const add = (x, y) => {
+  return x + y;
+};
+
+const result = add(2, 5);
+console.log(result);
+console.log("Penis");
